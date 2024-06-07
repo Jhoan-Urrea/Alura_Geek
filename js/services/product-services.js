9d8893c1cf7b95991ef7326d8e5f3601bdae231e
Jhoan-Urrea <js.urrea03@gmail.com>
2024-06-07 10:25:15 -0500

    reestructuracion de servicios para un mejor funcionamiento y correccion de errores

---
+++ js/services/product-services.js
@@ -0,0 +1,55 @@
+//  URL de tu API desplegada en Vercel
+const apiUrl = "https://fake-api-blush-tau.vercel.app/productos"; 
+
+// Funciones de la página web
+
+//Listar los productos 
+const productList = async () => {
+    try {
+        const res = await fetch(apiUrl);
+        return await res.json();
+    } catch (err) {
+        return console.log(err);
+    }
+};
+
+//Crear productos
+const createProducts = async (nombre, precio, imagen) => {
+    try {
+        const res = await fetch(apiUrl, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                nombre,
+                precio,
+                imagen,
+            }),
+        });
+        // Después de crear el producto, recarga la página
+        location.reload();
+        return await res.json();
+    } catch (err) {
+        return console.log(err);
+    }
+};
+
+//Eliminacion de un producto por id
+const deleteProduct = async (id) => {
+    try {
+        const res = await fetch(`${apiUrl}/${id}`, {
+            method: "DELETE"
+        });
+        return await res.json();
+    } catch (err) {
+        return console.log(err);
+    }
+};
+
+//Exportamos las funciones
+export const servicesProducts = {
+    productList,
+    createProducts,
+    deleteProduct,
+}
